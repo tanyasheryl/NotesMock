ca9d078d22fea19cbed3f95b423c830d1bcbeba9
Sheryl <TS051082@cerner.net>
2019-03-09 19:44:02 +0530

    messsges

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
-console.log('The value of PORT is:', process.env.MONGO_URI);
+//console.log('The value of PORT is:', process.env.MONGO_URI);
 module.exports = {
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientSecret: 'vRzJuSwyXkdMTWdPEo340fzB',
   googleClientID:
     '72305671294-hib0hhe1s0mlh6fnnrnkqn106qie6fri.apps.googleusercontent.com',
 
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  mongoURI: 'tans:tans123@ds119024.mlab.com:19024/paynotes-prod',
+  cookieKey: 'fjhfjfjfjhfJKFHJFH'
 };

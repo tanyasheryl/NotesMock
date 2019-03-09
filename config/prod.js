6261b74d0320d624b4e8e33ce6e93f2cb80b72ff
Sheryl <TS051082@cerner.net>
2019-03-09 19:20:38 +0530

    test2

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
-console.log('kikky');
-console.log('The value of PORT is:', process.env.PORT);
+console.log('The value of PORT is:', process.env.MONGO_URI);
 module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientID:
+    '72305671294-hib0hhe1s0mlh6fnnrnkqn106qie6fri.apps.googleusercontent.com',
 
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY

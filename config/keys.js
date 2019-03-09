5a4c44093400be83b51ecb598eb457b611509c44
Sheryl <TS051082@cerner.net>
2019-03-09 18:18:48 +0530

    keys file changed# t
    quit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-const express = require('express');
-if (process.envt.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
